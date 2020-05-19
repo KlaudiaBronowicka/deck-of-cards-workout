22c8356d899a92ff08ac88a15d9581d255ce4975
Klaudia <klaudia@indiespring.com>
2020-05-20 00:13:26 +0100

    Android app icon

--- DeckOfCards/DeckOfCards.Android/MainActivity.cs
+++ DeckOfCards/DeckOfCards.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DeckOfCards.Droid
 {
-    [Activity(Label = "DeckOfCards", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DeckOfCards", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
