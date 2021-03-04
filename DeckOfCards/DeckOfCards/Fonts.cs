048f8aaefcd73481cbe1989b073a8d955aa1c7f1
Klaudia Bronowicka <klaudia@indiespring.com>
2021-03-04 09:56:41 +0000

    Changed card font family and colors

---
+++ DeckOfCards/DeckOfCards/Fonts.cs
@@ -0,0 +1,5 @@
+﻿using Xamarin.Forms;
+
+[assembly: ExportFont("Nunito-Bold.ttf", Alias = "Nunito-Bold")]
+[assembly: ExportFont("Nunito-Light.ttf", Alias = "Nunito-Light")]
+[assembly: ExportFont("Nunito-Regular.ttf", Alias = "Nunito-Regular")]
