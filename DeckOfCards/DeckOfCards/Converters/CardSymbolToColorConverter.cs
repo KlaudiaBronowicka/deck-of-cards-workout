ac79c89daae2050842c8a37d62584b2756db14b1
Klaudia Bronowicka <klaudia@indiespring.com>
2021-03-13 13:31:08 +0000

    Used converter instead of data triggers for card symbol colors

---
+++ DeckOfCards/DeckOfCards/Converters/CardSymbolToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using DeckOfCards.Models;
+using Xamarin.Forms;
+
+namespace DeckOfCards.Converters
+{
+    public class CardSymbolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cardSymbol = (CardSymbol)Enum.Parse(typeof(CardSymbol), value.ToString());
+
+            return cardSymbol switch
+            {
+                CardSymbol.Hearts or CardSymbol.Diamond or CardSymbol.Joker => (Color)Application.Current.Resources["Accent"],
+                _ => (Color)Application.Current.Resources["Primary"],
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
